7d164070f3f4924fad86aa0b9de18a4948c2c4d7
S0lll0s <s0lll0s@blinkenshell.org>
2013-09-16 01:37:39 +0000

    Add asic idea of process in pyCloudForce.py

--- pyCloudForce.py
+++ pyCloudForce.py
@@ -1,5 +1,16 @@
 import mechanize
 import re
+from BeautifulSoup import BeautifulSoup
+
+class CloudForcer:
+    def __init__( self, proxies=None ):
+        self.browser = mechanize.Browser()
+        self.browser.set_proxies( proxies )
+
+    def upload( data, url ):
+        resp = browser.open( url )
+        soup = BeautifulSoup( browser.read() )
+        browser.select_form( predicate=(lambda x: (x.attrs['class'] == soup.textarea.findParent('form')['class']) if 'class' in x.attrs else False ) )
 
 
 if __name__ == "__main__":
