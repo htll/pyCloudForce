05e4290b6c8bc3be9b42ea9b6ab8388d8f6ffd56
Jon <mouseroot@mousetech.org>
2013-09-30 01:35:48 -0500

    Recreateing Main class

--- pyCloudForce.py
+++ pyCloudForce.py
@@ -2,6 +2,8 @@ import mechanize
 import re
 from BeautifulSoup import BeautifulSoup
 
+#   Verbose setup
+#   Basiclly break apart each function
 class CloudForcer:
     def __init__( self, proxies=None, judges=[(param, re.compile('(paste|text|input|data|comment)')) for param in ['name', 'id', 'class']] ):
         self.browser = mechanize.Browser()
@@ -23,13 +25,23 @@ class CloudForcer:
                 if regex.match( noodle[param] ):
                     valuable_soup.append( noodle )
         bowls = [ noodle.findParent('form') for noodle in useful_soup ]
-        bowls = bowls + [ bowl for bowl in browser.forms() for param, regex in self.noodle_judges if regex.match( bowl.[param] ) ]
+        bowls = bowls + [ bowl for bowl in browser.forms() for param, regex in self.noodle_judges if regex.match( bowl[param] ) ]
 
         # browser.select_form( predicate=(lambda x: (x.attrs['class'] == soup.textarea.findParent('form')['class']) if 'class' in x.attrs else False ) )
         return list( set( bowls ) )
 
+class CloudForcer2:
+    def __init__(self,proxies=None,special_attr=["name","id","class"]):
+        pass
+
+    def find_forms(self,url):
+        pass
+
 if __name__ == "__main__":
     import argparse
+    forcer = CloudForcer2()
+
+
     # argparse
     # save data
     # lel
