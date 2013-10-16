11aa52850bf3ab93720725bcc83643f40bd84a93
S0lll0s <S0lll0s@blinkenshell.org>
2013-10-16 21:52:57 +0200

    Add FieldJudge and TestForm, Replace CloudForcer by CloudForcer2, Reformat

--- pyCloudForce.py
+++ pyCloudForce.py
@@ -1,117 +1,108 @@
 import urllib
 import urllib2
 import re
+import base64
 from BeautifulSoup import BeautifulSoup
 
-class CloudForcer:
-    def __init__( self, proxies=None, judges=[(param, re.compile('(paste|text|input|data|comment)')) for param in ['name', 'id', 'class']] ):
-        self.browser = mechanize.Browser()
-        self.browser.set_proxies( proxies )
-        self.noodle_judges = judges
-
-    def save_data( self, data, url ):
-        forms = find_forms( url )
-
-    def find_forms( self, url ):
-        resp = self.browser.open( url )
-        soup = BeautifulSoup( resp.read() )
-
-        useful_soup = soup.findAll( "input" )
-        valuable_soup = soup.findAll( "textarea" ) # textareas are always valuable!
-
-        for noodle in useful_soup:
-            for param, regex in self.noodle_judges:
-                if regex.match( noodle[param] ):
-                    valuable_soup.append( noodle )
-        bowls = [ noodle.findParent('form') for noodle in useful_soup ]
-        bowls = bowls + [ bowl for bowl in browser.forms() for param, regex in self.noodle_judges if regex.match( bowl[param] ) ]
-
-        # browser.select_form( predicate=(lambda x: (x.attrs['class'] == soup.textarea.findParent('form')['class']) if 'class' in x.attrs else False ) )
-        return list( set( bowls ) )
-
 class CloudForm:
-    def __init__(self,method="get",action=None):
+    def __init__( self, action, method = "get", url = None, user_agent = 'Mozilla/6 (Unix/Linux 64bit) Gecko' ):
         self.method = method
         self.action = action
         self.inputs = {}
         self.submit = None
+        self.headers = { 'User-Agent': user_agent, 'Referer' : url }
 
-    def addInput(self,name=None,value=None):
-        self.inputs[name] = value
+    def addInput( self, name, value = None ):
+        self.inputs[ name ] = value
 
-    def addSubmit(self,name):
+    def addSubmit( self, name ):
         self.submit = name
 
-    def getSubmitString(self,values=[]):
+    def getSubmitString( self ):
         submitstr = {}
-        for kv in self.inputs:
-            #submitstr[key] = val
-            print kv
+        for key in self.inputs:
+            submitstr[key] = self.inputs[key]
         if self.submit:
-            submitstr[self.submit] = "Submit"
-        return urllib.urlencode(submitstr)
+            submitstr[self.submit] = 'Submit'
+        return urllib.urlencode( submitstr )
+
+    def submit():
+        data = getSubmitString()
+        if self.method == 'get':
+            self.action += '?' + data
+            data = None
+
+        response = urllib2.urlopen( urllib2.Request( self.action, data, self.headers ) )
+        if response.code != 200:
+            raise Exception( "HTTP Error (Status code {})".format( response.code ) )
+        pool = BeautifulSoup( response.read() )
+        return pool
+
+class TestForm( CloudForm ):
+    def __init__( self, action, method = 'get', url = None, user_agent = 'Mozilla/6 (Unix/Linux 64bit) Gecko' ):
+        super( self.__class__, self ).__init__( action, method, url, user_agent )
+        self.data = {}
+
+    def setInput( self, name, value = None ):
+        value = value | base64.b64encode( name )
+        self.data[ name ] = value
 
 
+class FieldJudge:
+    def __init__( self, parameters = ['name', 'id', 'class'], regex = re.compile('(paste|text|input|data|comment)') ):
+        self.params = parameters
+        self.regex = regex
 
-class CloudForcer2:
-    def __init__(self):
-        self.user_agent = "Mozilla/6 (Unix/Linux 64bit) Gecko"
-        self.headers = {"user-agent": self.user_agent}
-        self.form_attributes = ["name","id","class","method","action"]
+    def test( self, field ):
+        for param in self.params:
+            if regex.match( field[param] ):
+               return true
+        return false
 
-    def getForms(self,forms):
+    def testAll( self, things ):
+        return [ thing for thing in things if test( thing ) ]
+
+class CloudForcer:
+    def __init__( self, user_agent = 'Mozilla/6 (Unix/Linux 64bit) Gecko' ):
+        self.headers = { 'User-Agent': user_agent }
+        self.form_attributes = ['name', 'id', 'class', 'method', 'action']
+
+    def getForms( self, forms ):
         ret_forms = []
-        for i,form in enumerate(forms):
-            cForm = CloudForm()
-            for attribute in self.form_attributes:
-                try:
-                    cForm.method = form["method"]
-                    cForm.action = form["action"]
-                except KeyError,err:
-                    pass
+        return ret_forms
+
+    def find_forms( self, url ):
+        response = urllib2.urlopen( urllib2.Request( url, None, self.headers ) )
+        if response.code != 200:
+            raise Exception( "HTTP Error (Status code {})".format( response.code ) )
+        pool = BeautifulSoup( response.read() )
+
+        forms = []
+        for i, form in enumerate( pool.findAll( "form" ) ):
+            cForm = CloudForm( form['action'], form['method'], url, self.headers['User-Agent'] )
+            if form['method'] != 'post':
+                continue
             for child in form.contents:
                 try:
                     if child.name == "input" or child.name == "textarea" or child.name == "select":
-                        if child["type"] == "submit":
-                            cForm.addSubmit(child["name"])
-                        elif child["type"] == "hidden":
-                            cForm.addInput(child["name"],child["value"])
+                        if child['type'] == "submit":
+                            cForm.addSubmit( child['name'] )
                         else:
-                            cForm.addInput(name=child["name"])
-                except AttributeError,err:
+                            cForm.addInput( child['name'], child['value'] )
+                except AttributeError, err:
                     pass
-            ret_forms.append(cForm)
-        return ret_forms
+            forms.append( cForm )
 
-
-
-
-
-
-    def find_forms(self,url):
-        request = urllib2.Request(url,None,self.headers)
-        response = urllib2.urlopen(request)
-        page = response.read()
-        pool = BeautifulSoup(page)
-
-        #Find all forms
-        poolforms = pool.findAll("form")
-
-        #
-        forms = self.getForms(poolforms)
         for form in forms:
-            print "Method:",form.method
-            print "Action:",form.action
-            print form.getSubmitString(["Testing"])
-
-
-
+            print "Method:", form.method
+            print "Action:", form.action
+            print form.getSubmitString()
 
 
 if __name__ == "__main__":
 
-    forcer = CloudForcer2()
-    forcer.find_forms("http://pastebin.com/")
+    forcer = CloudForcer()
+    forcer.find_forms( "http://pastebin.com/" )
 
 
     # argparse
